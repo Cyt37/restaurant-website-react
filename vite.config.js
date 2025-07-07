c9cc7c1338854a42356d6f5a59c7c01763390afb
Cyt37 <116102994+Cyt37@users.noreply.github.com>
2025-07-07 00:22:21 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  // …
+  build: {
+    outDir: 'docs'
+  },
   base: '/restaurant-website-react/'
 });
